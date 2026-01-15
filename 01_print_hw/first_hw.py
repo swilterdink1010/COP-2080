b46ef46e6dc7576f43493df92ab39688273dc770
Sutton Wilterdink <swilterdink1010@floridapoly.edu>
2026-01-15 16:45:52 +0000

    Finished L01 HW - Print()

---
+++ 01_print_hw/first_hw.py
@@ -0,0 +1,23 @@
+# Program to test print()
+# 1/14/26
+
+my_name = "Sutton Wilterdink"
+print(my_name)
+
+# PEP 8 code layout statement 
+# [https://peps.python.org/pep-0008/#code-lay-out]
+
+print("Use 4 spaces per indentation level.")
+
+# Fact about the python language 
+# [https://docs.python.org/3.11/reference/import.html#the-path-based-finder]
+
+print("Python comes with several default meta path finders.")
+
+# Course outcomes (broken up as per PEP 8 guidelines)
+# [https://peps.python.org/pep-0008/#maximum-line-length]
+
+print("In this course I hope to learn more about " \
+        "fundamentals of coding, the pythonic way to write code, \n" \
+        "and how to solve different problems I might come across " \
+        "in the computer science industry.")
--- 01_print_hw/print_test.py
+++
@@ -1,6 +0,0 @@
-# Program to test print()
-# 1/14/26
-
-my_name = "Sutton Wilterdink"
-print(my_name)
-
